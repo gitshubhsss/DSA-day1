93da2d70d6d929287ef15d38e2ce33356169f0db
gitshubhsss <shubhamranjane16@gmail.com>
2024-07-01 19:58:45 +0530

    .\greaterTwo.java

---
+++ greaterTwo.java
@@ -0,0 +1,13 @@
+public class greaterTwo {
+    public static void main(String[] args) {
+        int a=5,b=8;
+        if(a>b){
+            System.out.println("a is greater than b");
+        } else if (a<b) {
+            System.out.println("b is greater than a");
+        }
+        else {
+            System.out.println("both are equls");
+        }
+    }
+}
