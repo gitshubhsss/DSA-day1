af5fdc7d3ff6569f86f07db40c60e998b8f4e4b5
gitshubhsss <shubhamranjane16@gmail.com>
2024-07-01 19:59:43 +0530

    .\sumOfNatural.java

---
+++ sumOfTwo.java
@@ -0,0 +1,7 @@
+public class sumOfTwo {
+    public static void main(String[] args) {
+        int a=5,b=5;
+        int sum=a+b;
+        System.out.println("sum of two numbers is "+sum);
+    }
+}
